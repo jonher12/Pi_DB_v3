0d527cfc6daf07fc0f0f69b6d93ab3a1f1493bb7
jonher12 <116646892+jonher12@users.noreply.github.com>
2025-05-23 16:44:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,13 +47,13 @@ if not st.session_state.logged_in:
         st.image("logo_rcm.png", width=120)
     
     with col2:
-        col_pi, col_title = st.columns([1, 7])  # Ajuste: mueve contenido levemente a la izquierda
+        col_pi, col_title = st.columns([1, 8])  # Ajuste: mueve contenido levemente a la izquierda
         with col_pi:
             st.image("pi.png", width=45)
         with col_title:
             st.markdown(
-                "<h1 style='margin: 0; padding-top: 10px; margin-left: -20px;'>Bienvenido a Pi DB v3</h1>",
-                unsafe_allow_html=True
+            "<h1 style='margin: 0; padding-top: 10px; margin-left: -30px;'>Bienvenido a Pi DB v3</h1>",
+            unsafe_allow_html=True
             )
     
     with col3:
