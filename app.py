3a5439983b1155061e4e62480aa092e9950ff18b
jonher12 <116646892+jonher12@users.noreply.github.com>
2025-05-23 16:24:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,8 @@ if not st.session_state.logged_in:
     # Encabezado con logos e ícono π
     col1, col2, col3 = st.columns([1, 2, 1])
     with col1:
-        st.markdown("<div style='margin-left: 20px;'><img src='logo_rcm.png' width='120'></div>", unsafe_allow_html=True)
+        st.write("")  # Esto crea un espacio en blanco
+        st.image("logo_rcm.png", width=120)
     with col2:
         col_pi, col_title = st.columns([1, 6])
         with col_pi:
