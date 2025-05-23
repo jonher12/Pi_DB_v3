8a218c7d00daffeee1b25eb0f3cac284f0c5555d
jonher12 <116646892+jonher12@users.noreply.github.com>
2025-05-23 16:25:19 -0400

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ if "logged_in" not in st.session_state:
 
 if not st.session_state.logged_in:
     # Encabezado con logos e ícono π
-    col1, col2, col3 = st.columns([1.5, 2, 1])
+    col1, col2, col3 = st.columns([.5, 2, 1])
     with col1:
         st.write("")  # Esto crea un espacio en blanco
         st.image("logo_rcm.png", width=120)
