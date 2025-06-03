25820d506b999654678850ec9969f889ee9f2fd0
jonher12 <116646892+jonher12@users.noreply.github.com>
2025-06-03 08:39:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,7 +53,7 @@ if not st.session_state.logged_in:
                 margin: 0;
                 padding-top: 10px;
                 font-size: 56px;
-                margin-left: 30px; # para centralizar el titulo, mientras mas grande y + mas a la derecha y viceversa
+                margin-left: 30px;  
                 text-align: right;
             '>Bienvenido a Pi v3</h1>
             """,
